a97c0aa6fcdf45d69c031f01ab4e600e5c627893
Weqaar Janjua <weqaar.a.janjua@intel.com>
2020-04-06 23:00:38 +0100

    v0.4 - mqtt, terminal, all sensors functional

--- kivy/ee513app/buildozer.spec
+++ kivy/ee513app/buildozer.spec
@@ -4,14 +4,14 @@ package.name = axlapp
 package.domain = ie.dcu.ee513
 source.dir = .
 source.include_exts = py,png,jpg,kv,atlas
-version = 0.3
+version = 0.4
 requirements = python3,kivy,plyer,requests,paho-mqtt,pyjnius,jnius
 icon.filename = %(source.dir)s/icons/dcu-logo-icon.png
 #Android
-orientation = portrait
+orientation = all
 author = Weqaar Janjua
 fullscreen = 0
-android.permissions = VIBRATE,INTERNET,ACTIVITY_RECOGNITION
+android.permissions = VIBRATE,INTERNET,ACTIVITY_RECOGNITION,ACCESS_FINE_LOCATION,ACCESS_COARSE_LOCATION
 android.arch = armeabi-v7a
 #IOS
 ios.kivy_ios_url = https://github.com/kivy/kivy-ios
--- kivy/ee513app/main.py
+++ kivy/ee513app/main.py
@@ -1,3 +1,10 @@
+"""
+Sensor Data Transmitter
+Transmits over MQTT, HTTP, HTTPS, UDP
+
+Author: Weqaar Janjua, <weqaar.janjua2@mail.dcu.ie>
+		EE513, DCU Electronic and Computer Engineering
+"""
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
@@ -11,18 +18,20 @@ from kivy.uix.popup import Popup
 from kivy.uix.spinner import Spinner
 from kivy import platform
 import paho.mqtt.client as mqtt
-from plyer import vibrator, accelerometer
+from plyer import vibrator, accelerometer, compass, gps, barometer, gravity, gyroscope
 import threading
 import time
 import queue
 import hashlib
+import requests
+import socket
 
 #setup graphics
 from kivy.config import Config
 Config.set('graphics','resizable',0)
  
 #Graphics fix
-from kivy.core.window import Window;
+from kivy.core.window import Window
 Window.clearcolor = (0,0,0,1.)
 
 class MainScreen(GridLayout):
@@ -31,8 +40,9 @@ class MainScreen(GridLayout):
 					 "port": None,
 					 "protocol": None,
 					 "topic_url": None,
+					 "sensor": None,				 
 					 "platform": str(platform)
-			   }
+				}
 		
 	def __init__(self, **kwargs):
 		super(MainScreen, self).__init__(**kwargs)
@@ -41,11 +51,15 @@ class MainScreen(GridLayout):
 		self.rows = 1
 
 		self._data_queue = queue.Queue()
+		self.gps_location = None
+		self.gps_status = None
+		self.default_sensor = "Accelerometer"
+		self.default_protocol = "terminal"
 
 		#Layouts
 		self._main_layout = BoxLayout(orientation='vertical', padding=0, size_hint=(1, 1))
 		_terminal_layout = BoxLayout(orientation='vertical', padding=0, size_hint=(1, 0.7))
-		_partition_layout = GridLayout(cols=2, rows=5, padding=0, size_hint=(1, 1), row_force_default=True, \
+		_partition_layout = GridLayout(cols=2, rows=6, padding=0, size_hint=(1, 1), row_force_default=True, \
 									   rows_minimum={0: 150, 1: 150, 2: 150, 3: 150}, row_default_height=150, spacing=25)
   
 		_action_previous = ActionPrevious(title='EE513 LABS', with_previous=False, app_icon='icons/chip.png', padding=0)
@@ -62,26 +76,32 @@ class MainScreen(GridLayout):
 		_action_bar.add_widget(_action_view)
 
 		_partition_layout.add_widget(Label(text='Server', size_hint_x=None, width=400))
-	
 		self.server = TextInput(text='192.168.0.20', multiline=False, cursor_blink=True)
 		self.server.bind(text=self.callback_server_text)
 		_partition_layout.add_widget(self.server)
 	
 		_partition_layout.add_widget(Label(text='Port', size_hint_x=None, width=400))
-	
 		self.port = TextInput(text='1883', multiline=False)
 		self.port.bind(text=self.callback_port_text)
 		_partition_layout.add_widget(self.port)
 
-		_partition_layout.add_widget(Label(text='Protocol', size_hint_x=None, width=400))
-
-		spinner = Spinner(
-					text='mqtt',
-					values=('http', 'mqtt'),
+		_partition_layout.add_widget(Label(text='Sensor', size_hint_x=None, width=400))
+		self._data_object["sensor"] = self.default_sensor
+		sensor_spinner = Spinner(
+					text=self.default_sensor,
+					values=('Accelerometer', 'Compass', 'GPS', 'Barometer', 'Gravity', 'Gyroscope'),
 					size_hint=(0.3, 0.5), sync_height=True)
+		sensor_spinner.bind(text=self.callback_sensor_spinner_text)
+		_partition_layout.add_widget(sensor_spinner)
 
-		spinner.bind(text=self.callback_spinner_text)
-		_partition_layout.add_widget(spinner)
+		_partition_layout.add_widget(Label(text='Protocol', size_hint_x=None, width=400))
+		self._data_object["protocol"] = self.default_protocol
+		protocol_spinner = Spinner(
+					text=self.default_protocol,
+					values=('http', 'https', 'mqtt', 'udp', 'terminal'),
+					size_hint=(0.3, 0.5), sync_height=True)
+		protocol_spinner.bind(text=self.callback_protocol_spinner_text)
+		_partition_layout.add_widget(protocol_spinner)
 
 		_partition_layout.add_widget(Label(text='Topic/URL', size_hint_x=None, width=400))
 		self.topic_url = TextInput(text='/ie/dcu/ee513', multiline=False)
@@ -106,10 +126,11 @@ class MainScreen(GridLayout):
 		if (value is True):
 			terminal_thread = threading.Thread(target=self.publish_terminal, args=(self.global_terminal, self._data_queue, ))
 			terminal_thread.start()
-			try:
-				load_jnius_nih()
-			except Exception as _exp:
-				self.global_terminal.insert_text("Exception: " + str(_exp) + "\n")
+			if (platform == 'android') or (platform == 'ios'):
+				try:
+					load_jnius_nih()
+				except Exception as _exp:
+					self.global_terminal.insert_text("Exception: " + str(_exp) + "\n")
 		elif (value is False):
 			self._data_queue.put("exit")
    
@@ -125,7 +146,7 @@ class MainScreen(GridLayout):
 		popup_box.add_widget(_content)
 		close_button = Button(text='close', size_hint=(0.2, 0.1), pos_hint={'center_x': .5, 'center_y': .5})
 		popup_box.add_widget(close_button)
-		__popup = Popup(title='Accelerometer Data Transmitter', title_align = 'center', \
+		__popup = Popup(title='Sensor Data Transmitter', title_align = 'center', \
 						content = popup_box, size_hint=(1, 1), auto_dismiss=False)
 		close_button.bind(on_press=__popup.dismiss)
 		__popup.open()
@@ -149,44 +170,341 @@ class MainScreen(GridLayout):
 	def callback_port_text(self, instance, value):
 		self._data_object["port"] = str(value)
 
-	def callback_spinner_text(self, spinner, text):
+	def callback_protocol_spinner_text(self, spinner, text):
 		self._data_object["protocol"] = text
+
+	def callback_sensor_spinner_text(self, spinner, text):
+		self._data_object["sensor"] = text
 
 	def callback_topic_url_text(self, instance, value):
 		self._data_object["topic_url"] = value
 
+	"""
+	src: https://github.com/kivy/plyer/blob/master/examples/gps/main.py
+	"""
+	def on_location(self, **kwargs):
+		self.gps_location = '\n'.join(['{}={}'.format(k, v) for k, v in kwargs.items()])
+	
+	"""
+	src: https://github.com/kivy/plyer/blob/master/examples/gps/main.py
+	"""
+	def on_status(self, stype, status):
+		self.gps_status = 'type={}\n{}'.format(stype, status)
+ 	
+	def xmit_payload(self, payload):
+		if (self._data_object.get("protocol") == "mqtt"):
+			mqttc = mqtt.Client()
+			mqttc.connect(self._data_object.get("server"), port=self._data_object.get("port"), keepalive=5)
+			ret_status = mqttc.publish(self._data_object.get("topic_url"), payload)
+			mqttc.disconnect()
+			return ret_status
+		elif (self._data_object.get("protocol") == "http"):
+			http_payload = { 'payload': payload }
+			ret_status = requests.post("http://" + self._data_object.get("server") + \
+       								   ":" + self._data_object.get("port") + self._data_object.get("topic_url"), data=http_payload) 
+			return ret_status
+		elif (self._data_object.get("protocol") == "https"):
+			https_payload = { 'payload': payload }
+			ret_status = requests.post("https://" + self._data_object.get("server") + \
+       								   ":" + self._data_object.get("port") + self._data_object.get("topic_url"), data=https_payload) 
+			return ret_status
+		elif (self._data_object.get("protocol") == "udp"):
+			sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+			sock.sendto(payload, (self._data_object.get("server"), self._data_object.get("port")))
+			return ret_status
+		else:
+			return False
+
+   
 	def publish_terminal(self, terminal, _data_queue):
 		terminal.insert_text("\n")
+  
 		if (platform == 'android') or (platform == 'ios'):
-			vibrator.vibrate(1)
 			try:
-				accelerometer.enable()
-			except Exception as _exp:
-				terminal.insert_text("Exception: " + str(_exp) + "\n")
-		previous_payload_hash = None	
-		while True:
-			if _data_queue.empty() is False:
-				qdata = _data_queue.get()
-				if qdata == "exit":
-					break
-			if (platform == 'android') or (platform == 'ios'):
-				if None in accelerometer.acceleration:
-					pass
-				else:
-					payload_hash = hashlib.md5(str(accelerometer.acceleration).encode('utf-8')).hexdigest()
-					if (previous_payload_hash is not None):
-						if (previous_payload_hash == payload_hash):
-							pass
-						else:
-							previous_payload_hash = payload_hash
-							terminal.insert_text("AXL (x, y, z): " + str(accelerometer.acceleration) + "\n")
-							time.sleep(0.1)
+				vibrator.vibrate(1)
+			except:
+				pass
+
+			#Accelerometer
+			if self._data_object.get("sensor") == "Accelerometer":
+				try:
+					accelerometer.enable()
+				except Exception as _exp:
+					terminal.insert_text("Exception: " + str(_exp) + "\n")
+					return
+				previous_payload_hash = None	
+				while True:
+					if _data_queue.empty() is False:
+						qdata = _data_queue.get()
+						if qdata == "exit":
+							try:
+								accelerometer.disable()
+							except:
+								pass
+							break
+					if None in accelerometer.acceleration:
+						pass
 					else:
-						terminal.insert_text("AXL (x, y, z): " + str(accelerometer.acceleration) + "\n")
-						time.sleep(0.1)
+						payload_hash = hashlib.md5(str(accelerometer.acceleration).encode('utf-8')).hexdigest()
+						if (previous_payload_hash is not None):
+							if (previous_payload_hash == payload_hash):
+								pass
+							else:
+								previous_payload_hash = payload_hash
+								payload = "(x, y, z): " + str(accelerometer.acceleration)
+								terminal.insert_text(payload + "\n")
+								if (self._data_object.get("protocol") != "terminal"):
+									try:
+										terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+									except Exception as _exp:
+										terminal.insert_text("Exception: " + str(_exp) + "\n")
+										return
+								time.sleep(0.1)
+						else:
+							payload = "(x, y, z): " + str(accelerometer.acceleration)
+							terminal.insert_text(payload + "\n")
+							if (self._data_object.get("protocol") != "terminal"):
+								try:
+									terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+								except Exception as _exp:
+									terminal.insert_text("Exception: " + str(_exp) + "\n")
+									return
+							time.sleep(0.1)
+			#Compass
+			elif self._data_object.get("sensor") == "Compass":
+				try:
+					compass.enable()
+				except Exception as _exp:
+					terminal.insert_text("Exception: " + str(_exp) + "\n")
+					return
+				previous_payload_hash = None	
+				while True:
+					if _data_queue.empty() is False:
+						qdata = _data_queue.get()
+						if qdata == "exit":
+							try:
+								compass.disable()
+							except:
+								pass
+							break
+					if None in compass.field:
+						pass
+					else:
+						payload_hash = hashlib.md5(str(compass.field).encode('utf-8')).hexdigest()
+						if (previous_payload_hash is not None):
+							if (previous_payload_hash == payload_hash):
+								pass
+							else:
+								previous_payload_hash = payload_hash
+								payload = "(x, y, z): " + str(compass.field)
+								terminal.insert_text(payload + "\n")
+								if (self._data_object.get("protocol") != "terminal"):
+									try:
+										terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+									except Exception as _exp:
+										terminal.insert_text("Exception: " + str(_exp) + "\n")
+										return
+								time.sleep(0.1)
+						else:
+							payload = "(x, y, z): " + str(compass.field)
+							terminal.insert_text(payload + "\n")
+							if (self._data_object.get("protocol") != "terminal"):
+								try:
+									terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+								except Exception as _exp:
+									terminal.insert_text("Exception: " + str(_exp) + "\n")
+									return
+							time.sleep(0.1)
+			#GPS
+			elif self._data_object.get("sensor") == "GPS":
+				try:
+					gps.configure(on_location=self.on_location, on_status=self.on_status)
+					gps.start(1000, 0)
+				except Exception as _exp:
+					terminal.insert_text("Exception: " + str(_exp) + "\n")
+					return
+				previous_payload_hash = None	
+				while True:
+					if _data_queue.empty() is False:
+						qdata = _data_queue.get()
+						if qdata == "exit":
+							try:
+								gps.stop()
+							except:
+								pass
+							break
+					if self.gps_location is None:
+						pass
+					else:
+						payload_hash = hashlib.md5(str(self.gps_location).encode('utf-8')).hexdigest()
+						if (previous_payload_hash is not None):
+							if (previous_payload_hash == payload_hash):
+								pass
+							else:
+								previous_payload_hash = payload_hash
+								payload = str(self.gps_location)
+								terminal.insert_text(payload + "\n")
+								if (self._data_object.get("protocol") != "terminal"):
+									try:
+										terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+									except Exception as _exp:
+										terminal.insert_text("Exception: " + str(_exp) + "\n")
+										return
+								time.sleep(3)
+						else:
+							payload = str(self.gps_location)
+							terminal.insert_text(payload + "\n")
+							if (self._data_object.get("protocol") != "terminal"):
+								try:
+									terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+								except Exception as _exp:
+									terminal.insert_text("Exception: " + str(_exp) + "\n")
+									return
+							time.sleep(0.5)
+			#Barometer
+			elif self._data_object.get("sensor") == "Barometer":
+				try:
+					barometer.enable()
+				except Exception as _exp:
+					terminal.insert_text("Exception: " + str(_exp) + "\n")
+					return
+				previous_payload_hash = None	
+				while True:
+					if _data_queue.empty() is False:
+						qdata = _data_queue.get()
+						if qdata == "exit":
+							try:
+								barometer.disable()
+							except:
+								pass
+							break
+					if barometer.pressure is None:
+						pass
+					else:
+						payload_hash = hashlib.md5(str(barometer.pressure).encode('utf-8')).hexdigest()
+						if (previous_payload_hash is not None):
+							if (previous_payload_hash == payload_hash):
+								pass
+							else:
+								previous_payload_hash = payload_hash
+								payload = "hPa: " + str(barometer.pressure)
+								terminal.insert_text(payload + "\n")
+								if (self._data_object.get("protocol") != "terminal"):
+									try:
+										terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+									except Exception as _exp:
+										terminal.insert_text("Exception: " + str(_exp) + "\n")
+										return
+								time.sleep(0.1)
+						else:
+							payload = "hPa: " + str(barometer.pressure)
+							terminal.insert_text(payload + "\n")
+							if (self._data_object.get("protocol") != "terminal"):
+								try:
+									terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+								except Exception as _exp:
+									terminal.insert_text("Exception: " + str(_exp) + "\n")
+									return
+			#Gravity
+			elif self._data_object.get("sensor") == "Gravity":
+				try:
+					gravity.enable()
+				except Exception as _exp:
+					terminal.insert_text("Exception: " + str(_exp) + "\n")
+					return
+				previous_payload_hash = None	
+				while True:
+					if _data_queue.empty() is False:
+						qdata = _data_queue.get()
+						if qdata == "exit":
+							try:
+								gravity.disable()
+							except:
+								pass
+							break
+					if None in gravity.gravity:
+						pass
+					else:
+						payload_hash = hashlib.md5(str(gravity.gravity).encode('utf-8')).hexdigest()
+						if (previous_payload_hash is not None):
+							if (previous_payload_hash == payload_hash):
+								pass
+							else:
+								previous_payload_hash = payload_hash
+								payload = "(x, y, z): " + str(gravity.gravity)
+								terminal.insert_text(payload + "\n")
+								if (self._data_object.get("protocol") != "terminal"):
+									try:
+										terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+									except Exception as _exp:
+										terminal.insert_text("Exception: " + str(_exp) + "\n")
+										return
+								time.sleep(0.1)
+						else:
+							payload = "(x, y, z): " + str(gravity.gravity)
+							terminal.insert_text(payload + "\n")
+							if (self._data_object.get("protocol") != "terminal"):
+								try:
+									terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+								except Exception as _exp:
+									terminal.insert_text("Exception: " + str(_exp) + "\n")
+									return
+			#Gyroscope
+			elif self._data_object.get("sensor") == "Gyroscope":
+				try:
+					gyroscope.enable()
+				except Exception as _exp:
+					terminal.insert_text("Exception: " + str(_exp) + "\n")
+					return
+				previous_payload_hash = None	
+				while True:
+					if _data_queue.empty() is False:
+						qdata = _data_queue.get()
+						if qdata == "exit":
+							try:
+								gyroscope.disable()
+							except:
+								pass
+							break
+					if None in gyroscope.rotation:
+						pass
+					else:
+						payload_hash = hashlib.md5(str(gyroscope.rotation).encode('utf-8')).hexdigest()
+						if (previous_payload_hash is not None):
+							if (previous_payload_hash == payload_hash):
+								pass
+							else:
+								previous_payload_hash = payload_hash
+								payload = "(x, y, z): " + str(gyroscope.rotation)
+								terminal.insert_text(payload + "\n")
+								if (self._data_object.get("protocol") != "terminal"):
+									try:
+										terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+									except Exception as _exp:
+										terminal.insert_text("Exception: " + str(_exp) + "\n")
+										return
+								time.sleep(0.1)
+						else:
+								payload = "(x, y, z): " + str(gyroscope.rotation)
+								terminal.insert_text(payload + "\n")
+								if (self._data_object.get("protocol") != "terminal"):
+									try:
+										terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+									except Exception as _exp:
+										terminal.insert_text("Exception: " + str(_exp) + "\n")
+										return
 			else:
-				terminal.insert_text("time: " + str(time.strftime("%c", time.gmtime())) + "\n")
-				time.sleep(2)
+				pass
+		else:
+			payload = "time: " + str(time.strftime("%c", time.gmtime()))
+			terminal.insert_text(payload + "\n")
+			if (self._data_object.get("protocol") != "terminal"):
+				try:
+					terminal.insert_text("xmit status: " + self.xmit_payload(payload) + "\n")
+				except Exception as _exp:
+					terminal.insert_text("Exception: " + str(_exp) + "\n")
+					return
+			time.sleep(2)
 
 #Bug fix, src: https://github.com/jeremyklelifa/Jukebar/commit/0607c7cb4dcef2125ab1b2b70227a7999f7caabe
 #Another potential solution: https://github.com/VoiceThread/pyjnius
